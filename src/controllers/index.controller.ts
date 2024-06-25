d5fbf320f391c7f2a3b3ddb8839d080803073848
rapmue <rapmue@gmail.com>
2024-06-26 00:10:16 +0200

    Fix typos (#3)
    * fixed typos
    
    * also fixed typo in name of lock file

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 export const wellcome = async (req: Request, res: Response) => {
   try {
-    res.json({ message: "Wellcome to the FestivalPOS API" });
+    res.json({ message: "Welcome to the FestivalPOS API" });
   } catch (error) {
     res.status(500).json({ message: "Could not return json" });
   }
